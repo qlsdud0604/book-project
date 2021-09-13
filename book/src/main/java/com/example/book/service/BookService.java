b17f73c6d4a2c0975e8bc1ba3b03034845f2fb3c
임빈영 <qlsdud960604@gmail.com>
2021-09-13 15:28:55 +0900

    Service 영역 구현

--- book/src/main/java/com/example/book/service/BookService.java
+++ book/src/main/java/com/example/book/service/BookService.java
@@ -1,7 +1,50 @@
 package com.example.book.service;
 
+import com.example.book.domain.Book;
+import com.example.book.domain.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.util.Supplier;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Service
 public class BookService {
+
+    private final BookRepository bookRepository;
+
+    @Transactional
+    public Book registerBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Transactional(readOnly = true)
+    public Book getBookDetail(Long id) {
+        return bookRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("id를 확인해주세요."));
+    }
+
+    public List<Book> getBookList() {
+        return bookRepository.findAll();
+    }
+
+    @Transactional
+    public Book updateBook(Long id, Book book) {
+
+        Book bookEntity = bookRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("id를 확인해주세요."));
+
+        bookEntity.setTitle(book.getTitle());
+        bookEntity.setAuthor(book.getAuthor());
+
+        return bookEntity;   // 함수 종료 -> 트랜잭션 종료 -> 영속화 되어있는 데이터를 DB로 갱신(flush) -> commit ===> 이러한 방식을 더티체킹이라 함
+    }
+
+    @Transactional
+    public String deleteBook(Long id) {
+        bookRepository.deleteById(id);
+        return "삭제되었습니다.";
+    }
 }
